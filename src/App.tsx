e59c5d9ae3b38c88e22786eedbcbb1cc297d361a
Garrit Franke <garrit@slashdev.space>
2025-09-28 19:46:32 +0200

    Add cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "cardsJS/cards.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="play-area">
+      <div className="hand hhand active-hand room-hand">
+        <img className="card" src="cards/AS.svg" />
+        <img className="card" src="cards/KS.svg" />
+        <img className="card" src="cards/QS.svg" />
+        <img className="card" src="cards/JS.svg" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="hand hhand-compact weapon-hand">
+        <img className="card" src="cards/AS.svg" />
+        <img className="card" src="cards/KS.svg" />
+        <img className="card" src="cards/QS.svg" />
+        <img className="card" src="cards/JS.svg" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
